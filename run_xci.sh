95f705556a935b76db4cd9989ada6b7991d2c468
Joseph Moore <moore.joseph002@gmail.com>
2024-07-02 12:46:17 +0000

    Fixing specialfiles bug

--- run_xci.sh
+++ run_xci.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #PBS -q arm
 #PBS -l select=10
-#PBS -l walltime=24:00:00
+#PBS -l walltime=6:00:00
 
 module load cray-python/3.8.5.1
 module use ~/modules/modulefiles
 module load openmpi-4.1.6
+pip install psutil
 
 aprun -n 640 $HOME/simeng-parameter-study/xci_launcher.sh
